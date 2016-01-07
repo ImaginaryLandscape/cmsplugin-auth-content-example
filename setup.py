07f9241e8d8bc831c9b36456ad63abd7ddedac53
David Bertrand <dbertrand@imagescape.com>
2016-01-07 17:57:51 +0000

    Added setup, manifest, and gitignore

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cmsplugin_auth_content',
+    version='0.0.1',
+    packages=['cmsplugin_auth_content'],
+    include_package_data=True,
+)
